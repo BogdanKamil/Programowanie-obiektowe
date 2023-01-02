eeb3ed5d85279022e8cc3c8e966ea6b442bb8c77
BogdanKamil <115534921+BogdanKamil@users.noreply.github.com>
2023-01-02 19:39:54 +0100

    Create Laboratoria7.java

---
+++ Laboratoria7.java
@@ -0,0 +1,80 @@
+public class Main
+{
+    public static long silnia(int n) throws BlednaWartoscDlaSilniException
+    {
+        if (n < 0)
+        {
+            throw new BlednaWartoscDlaSilniException("Number cannot be negative");
+        }
+
+        long result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            int number = 5;
+            long factorial = silnia(number);
+            System.out.println(number + "! = " + factorial);
+        }
+        catch (BlednaWartoscDlaSilniException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+class BlednaWartoscDlaSilniException extends Exception
+{
+    public BlednaWartoscDlaSilniException(String message)
+    {
+        super(message);
+    }
+}
+-------------------------------------------------------------------------
+
+public class Main
+{
+    public static long silnia(int n) throws BlednaWartoscDlaSilniException
+    {
+        if (n < 0)
+        {
+            throw new BlednaWartoscDlaSilniException("Number cannot be negative");
+        }
+
+        long result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            int number = 5;
+            long factorial = silnia(number);
+            System.out.println(number + "! = " + factorial);
+        }
+        catch (BlednaWartoscDlaSilniException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+class BlednaWartoscDlaSilniException extends Exception
+{
+    public BlednaWartoscDlaSilniException(String message)
+    {
+        super(message);
+    }
+}
